ae1d1731fcbdea4a49a53ea282c029c2c7c05ac3
durumi-contact <durumi-contact@gmail.com>
2025-08-25 16:52:45 +0900

    Make Modal

--- assets/js/construction.js
+++ assets/js/construction.js
@@ -1,3 +1,19 @@
+const modal = document.querySelector('.modal')
+const modalClose = document.querySelector('.confirm-btn')
+
+function ShowModal() {
+    modal.style.display = 'block';
+
+    modalClose.addEventListener('click',function() {
+        modal.style.display = 'none';
+        showText();
+    // 텍스트 애니메이션 후 이미지 애니메이션 실행
+    setTimeout(() => {
+        showImage();
+    }, 500); // 0.5초 후 이미지 애니메이션 시작
+    })
+};
+
 function showText() {
     const textDisplay = document.getElementById('buildingTextDisplay');
     textDisplay.innerHTML = '국립경국대학교';
@@ -17,10 +33,6 @@ function showImage() {
 
 // 페이지 로드 시 자동으로 텍스트와 이미지 표시
 document.addEventListener('DOMContentLoaded', function() {
-    showText();
-    // 텍스트 애니메이션 후 이미지 애니메이션 실행
-    setTimeout(() => {
-        showImage();
-    }, 500); // 0.5초 후 이미지 애니메이션 시작
+    ShowModal();
 });
 
